5b1ad0891c3b6ee9c1fb45b852d3598ece08deaa
chen0401 <chentravelling@163.com>
2021-03-01 11:08:56 +0800

    No.35

---
+++ src/main/java/com/c/java/leetcode/A35searchInsert.java
@@ -0,0 +1,21 @@
+package com.c.java.leetcode;
+
+/**
+ * @author liufan
+ * @date 2021/3/1 10:56
+ **/
+public class A35searchInsert {
+    public static void main(String[] args) {
+        System.out.println(new A35searchInsert().searchInsert(new int[]{1, 2, 3, 5, 6}, 9));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        for (; i < nums.length; i++) {
+            if (target <= nums[i]) {
+                break;
+            }
+        }
+        return i;
+    }
+}
