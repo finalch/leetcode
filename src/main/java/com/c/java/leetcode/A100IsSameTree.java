496b079d4a05b86a2fda9c4fcaac1a95a2a26854
chen0401 <chentravelling@163.com>
2021-03-02 11:51:00 +0800

    add 100

---
+++ src/main/java/com/c/java/leetcode/A100IsSameTree.java
@@ -0,0 +1,26 @@
+package com.c.java.leetcode;
+
+import com.c.java.leetcode.common.Helper;
+import com.c.java.leetcode.common.TreeNode;
+
+/**
+ * @author liufan
+ * @date 2021/3/2 11:31
+ **/
+public class A100IsSameTree {
+    public static void main(String[] args) {
+        TreeNode p = Helper.initTreeNodes(new int[]{1, 0, 3});
+        TreeNode q = Helper.initTreeNodes(new int[]{1, 2, 3});
+        System.out.println(new A100IsSameTree().isSameTree(p, q));
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
